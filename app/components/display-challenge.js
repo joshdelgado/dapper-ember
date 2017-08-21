7e095f1a19295cc62eab62fef6ecd09fc93c7063
jdislawlz <accounts@joshdelgado.me>
2017-08-21 14:30:24 -0400

    Display challenges
    Added button to display challenges from a JSON file

--- app/components/display-challenge.js
+++ app/components/display-challenge.js
@@ -1,9 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  challenge: ['Josh', 'Derek', 'Stephen', 'Pat'],
+  name: 'nameee',
+  description: 'descriptionnnn',
+  image: 'imageeeee',
   actions: {
     newChallenge: function(){
+      var thiss = this;
+      $.getJSON("/challenge-packs/bahpack1.json", function(data){
+        var num = Math.floor(Math.random() * data['challenges'].length)
+        var challenge = data['challenges'][num];
+
+        thiss.set('name', challenge.name);
+        thiss.set('description', challenge.description);
+        thiss.set('image', challenge.image);
+      });
     }
   }
 });
--- app/components/player-list.js
+++ app/components/player-list.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  players: ['Josh', 'Derek', 'Stephen', 'Pat'],
+  players: [
+    { name: 'Josh', score: 0 },
+    { name: 'Derek', score: 0 },
+    { name: 'Stephen', score: 0 },
+    { name: 'Pat', score: 0 }
+  ],
   actions: {
     addPlayer: function(){
       let playerName = this.get('playerName');
