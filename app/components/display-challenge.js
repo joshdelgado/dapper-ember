5f0ca2663dc39ca989b567aa433142081030ec78
jdislawlz <accounts@joshdelgado.me>
2017-08-23 17:09:26 -0400

    Styles and challenge presentation
    Added more styles so it looks like a real thing now. Also challenges
    have images now

--- app/components/display-challenge.js
+++ app/components/display-challenge.js
@@ -1,21 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  name: 'nameee',
-  description: 'descriptionnnn',
-  image: 'imageeeee',
+  name: '',
+  description: '',
+  image: '',
   actions: {
     newChallenge: function(){
-      var _this = this;
-      $.getJSON("/challenge-packs/bahpack1.json", function(data){
-        var num = Math.floor(Math.random() * data['challenges'].length)
-        var challenge = data['challenges'][num];
+      let _this = this;
+      $.getJSON("/challenge-packs/hellohollywood.json", function(data){
+        let num = Math.floor(Math.random() * data['challenges'].length),
+            challenge = data['challenges'][num];
 
         _this.set('name', challenge.name);
         _this.set('description', challenge.description);
         _this.set('image', challenge.image);
-
-        
       });
     }
   }
